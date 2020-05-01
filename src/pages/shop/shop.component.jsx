db7fa992b2ef5734a0073d772842634dd6d4087e
hnekhniadovich <hanna@nextguest.com>
2020-05-01 02:09:25 -0400

    add directory reducer, add shop reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,11 @@
-import React, { Component } from 'react';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-import SHOP_DATA from './shop.data';
+import React from 'react';
 
-class ShopPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            collections: {
-                SHOP_DATA
-            }
-        }
-    }
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-    render() {
-        const { collections } = this.state;
-        
-        return (
-            <div className='shop-page'>
-                {collections.SHOP_DATA.map(({ id, ...otherCollectionProps }) => (
-                    <CollectionPreview key={id} {...otherCollectionProps} />
-                ))}
-            </div>
-        )
-    }
-}
-
+const ShopPage = () => (
+    <div className='shop-page'>
+        <CollectionsOverview />
+    </div>  
+);
+    
 export default ShopPage;
